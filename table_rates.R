7673f3e3d30668626d6bf80c0ca672edb4e05a55
duhi23 <diego.21.phs@hotmail.com>
2016-10-05 13:27:33 +0200

    Update

--- table_rates.R
+++ table_rates.R
@@ -2,15 +2,37 @@
 ##########     Interés instantaneo - nominal     ##########
 ###########################################################
 
+# Interés instantáneo
+rt <- function(t){
+      return(0.01*t+0.05)
+}
+
 # Función descuento
 vt <- function(t){
       return(exp(-0.005*t^2-0.05*t))
 }
 
-# Tiempo 
+# Tiempo
 t <- seq(0,50)
-# Capital
+
+# Capital inicial
 C0 <- 100000
 
-vt(t)
+# Capital futuro
+Cf <- C0*vt(t)
+
+# Tabla consolidada
+options(digits = 6)
+data.frame(Tiempo=t, IntNominal=rt(t), CapFinal=Cf)
+
+
+
+##### Funcion de supervivencia #####
+
+t <- seq(0,120, length.out = 121)
+Ft <- 1-(1-t/120)^(1/6)
+St <- 1-Ft
+
+
+
 
